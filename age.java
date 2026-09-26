eae5fa5204c094e6ad10d56d00fc2939d9625fe7
kalyan <gosukalyan2032@gmail.com>
2026-09-26 15:13:34 +0530

    Add java files

---
+++ age.java
@@ -0,0 +1,17 @@
+public class  age {
+    public static void main(String[] args) {
+
+        int age = 17;
+
+        if(age >= 18){
+            System.out.println("ELIGIBLE");
+        } else {
+            System.out.println("YOU ARE NOT ELIGIBLE");
+
+
+        }
+        }
+
+    }
+    
+
---
+++ sum.java
@@ -0,0 +1,14 @@
+class sum {
+    static int add(int a, int b) {
+        int sum = a+ b;
+        return sum;
+
+    }
+       public static void main(String[] args) {
+        int result = add(10,20);
+        System.out.println("Sum of two numbers is: " + result);
+       }
+
+       
+
+    }
